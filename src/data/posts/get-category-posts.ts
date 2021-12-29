4fa9794a7e39389883a517d9154a917222ed936d
italo macedo <italomacedoo@hotmail.com>
2021-12-29 13:27:42 -0300

    tirando category

--- src/data/posts/get-category-posts.ts
+++
@@ -1,8 +0,0 @@
-import { PostData } from '../../domain/posts/post';
-import { fetchJson } from '../../utils/fetch-json';
-
-export const getCategoryPosts = async (query = ''): Promise<PostData[]> => {
-  const url = `https://protected-coast-85961.herokuapp.com/api/posts?${query}`;
-  const posts = await fetchJson<PostData[]>(url);
-  return posts;
-};
